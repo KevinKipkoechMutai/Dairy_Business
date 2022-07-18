510ebbcfe072cac408ed7e1dae85cf30c4486488
Kevin <kevin.kipkoech@student.moringaschool.com>
2022-07-18 05:09:17 +0300

    This will implement the summer function

--- JS/index.js
+++ JS/index.js
@@ -1,7 +1,12 @@
 
-var milk1 = 
+let milk1 = document.getElementById('shadeData1');
+let milk2 = document.getElementById('shadeData2');
+let milk3 = document.getElementById('shadeData3');
+let milk4 = document.getElementById('shadeData4');
+let milk5 = document.getElementById('shadeData5');
+let milk6 = document.getElementById('shadeData6');
 
 
-const function milkSummer() {
-    
+function milkSummer() {
+    document.getElementById('submit').innerHTML="The total milk produced today is liters.";
 }
