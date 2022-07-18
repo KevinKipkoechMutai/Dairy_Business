b14d7945ff4740b9f21cf736420f4500e1710d85
Kevin <kevin.kipkoech@student.moringaschool.com>
2022-07-18 11:51:47 +0300

    This will change the Javascript code

--- JS/index.js
+++ JS/index.js
@@ -10,15 +10,15 @@ data[5] = data['shadeData6']=parseInt(document.getElementById('shadeData6').valu
 
 let totalSum = data.shadeData1+data.shadeData2+data.shadeData3+data.shadeData4+data.shadeData5+data.shadeData6;
 
-document.getElementById('formData').innerHTML += '<Br>'
+document.getElementById('formData').innerHTML += '<Br>';
 
 
-document.getElementById('formData').innerHTML += "<p>Your production from Shed A is " + data.shadeData1 +" liters today</p>"
-document.getElementById('formData').innerHTML += "<p>Your production from Shed B is " + data.shadeData2 +" liters today</p>"
-document.getElementById('formData').innerHTML += "<p>Your production from Shed C is " + data.shadeData3 +" liters today</p>"
-document.getElementById('formData').innerHTML += "<p>Your production from Shed D is " + data.shadeData4 +" liters today</p>"
-document.getElementById('formData').innerHTML += "<p>Your production from Shed E is " + data.shadeData5 +" liters today</p>"
-document.getElementById('formData').innerHTML += "<p>Your production from Shed F is " + data.shadeData6 +" liters today</p>"
+document.getElementById('formData').innerHTML += "<p>Your production from Shed A is " + data.shadeData1 +" liters today</p>";
+document.getElementById('formData').innerHTML += "<p>Your production from Shed B is " + data.shadeData2 +" liters today</p>";
+document.getElementById('formData').innerHTML += "<p>Your production from Shed C is " + data.shadeData3 +" liters today</p>";
+document.getElementById('formData').innerHTML += "<p>Your production from Shed D is " + data.shadeData4 +" liters today</p>";
+document.getElementById('formData').innerHTML += "<p>Your production from Shed E is " + data.shadeData5 +" liters today</p>";
+document.getElementById('formData').innerHTML += "<p>Your production from Shed F is " + data.shadeData6 +" liters today</p>";
 
 
 }
